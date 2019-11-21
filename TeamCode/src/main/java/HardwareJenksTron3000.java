8e564fc9424333dd89bb6ffdf8a3e8336b3fb5a1
London Jenks <londonjenks@gmail.com>
2019-11-21 09:58:29 -0700

    Added servo controller for foundation

---
+++ TeamCode/src/main/java/HardwareJenksTron3000.java
@@ -0,0 +1,139 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ */
+public class HardwareJenksTron3000
+{
+    /* Public OpMode members. */
+    public DcMotor  leftDrive   = null;
+    public DcMotor  rightDrive  = null;
+    public DcMotor  frontDrive  = null;
+    public DcMotor  backDrive   = null;
+    public Servo    foundation1  = null;
+    public Servo    foundation2 = null;
+
+    public static final double FOUNDATION1_START =  0.5 ;
+    public static final double FOUNDATION1_GRAB  =  0.45 ;
+
+    public static final double FOUNDATION2_START =  0.45 ;
+    public static final double FOUNDATION2_GRAB  =  0.5 ;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public HardwareJenksTron3000(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        leftDrive   = hwMap.get(DcMotor.class, "left_drive");
+        rightDrive  = hwMap.get(DcMotor.class, "right_drive");
+        frontDrive  = hwMap.get(DcMotor.class, "front_drive");
+        backDrive   = hwMap.get(DcMotor.class, "back_drive");
+
+        //Set motor directions
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontDrive.setDirection(DcMotor.Direction.FORWARD);
+        backDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        // Set all motors to zero power
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        frontDrive.setPower(0);
+        backDrive.setPower(0);
+
+        // Set all drive motors to run using encoders.
+        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        // Define and initialize ALL installed servos.
+        foundation1  = hwMap.get(Servo.class, "foundation1");
+        foundation1.setPosition(FOUNDATION1_START);
+        foundation2  = hwMap.get(Servo.class, "foundation2");
+        foundation1.setPosition(FOUNDATION2_START);
+
+
+
+
+    }
+    //drive forward and backward
+    public void driveForwardBackward (double power){
+        leftDrive.setPower(-power/1.5);
+        rightDrive.setPower(power/1.5);
+        frontDrive.setPower(0.0);
+        backDrive.setPower(0.0);
+    }
+
+    //stop all motors
+    public void driveNotAtAll (){
+        leftDrive.setPower(0.0);
+        rightDrive.setPower(0.0);
+        frontDrive.setPower(0.0);
+        backDrive.setPower(0.0);
+    }
+
+    //drive left and right
+    public void driveRightLeft (double power) {
+        leftDrive.setPower(0.0);
+        rightDrive.setPower(0.0);
+        frontDrive.setPower(power/1.5);
+        backDrive.setPower(-power/1.5);
+    }
+
+    //rotate clockwise and counter clockwise
+    public void rotate (double power){
+        leftDrive.setPower(power/4.0);
+        rightDrive.setPower(power/4.0);
+        frontDrive.setPower(power/4.0);
+        backDrive.setPower(power/4.0);
+    }
+
+}
+
---
+++ TeamCode/src/main/java/JenksTron_TeleOp.java
@@ -0,0 +1,97 @@
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+
+@TeleOp(name="JenksTron: TeleOp", group="TeleOp")
+
+public class JenksTron_TeleOp extends OpMode {
+
+    HardwareJenksTron3000 robot = new HardwareJenksTron3000(); //use the class to define JenksTron's hardware
+
+
+    @Override
+    public void init() {
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Driver");    //
+    }
+
+
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+        // threshold for dead space on joysticks
+        double THRESHOLD = 0.05;
+
+
+        //************************* DRIVING THE ROBOT *************************************
+
+        // left stick to move forward and backward
+        if(Math.abs(gamepad1.left_stick_y) > THRESHOLD) {
+            robot.driveForwardBackward(gamepad1.left_stick_y);
+
+            // allows right and left motion to be combined
+            if(Math.abs(gamepad1.left_stick_x)> THRESHOLD){
+                robot.driveRightLeft(gamepad1.left_stick_x);
+                robot.driveForwardBackward(gamepad1.left_stick_y);
+            }
+        }
+
+        // left stick to move right and left
+        else if(Math.abs(gamepad1.left_stick_x)> THRESHOLD){
+            robot.driveRightLeft(gamepad1.left_stick_x);
+
+            //allows forward and backward motion to be combined
+            if(Math.abs(gamepad1.left_stick_y)> THRESHOLD){
+                robot.driveForwardBackward(gamepad1.left_stick_y);
+                robot.driveRightLeft(gamepad1.left_stick_x);
+            }
+        }
+
+        //rotates the robot with joystick one right stick x direction
+        else if (Math.abs(gamepad1.right_stick_x) > THRESHOLD){
+            robot.rotate(gamepad1.right_stick_x);
+        }
+
+        // if no joys on player one all wheels stop
+        else{
+            robot.driveNotAtAll();
+        }
+
+        //********************** CONTROLLING ROBOT PERIPHERIALS *************************
+
+        //control the foundation moving servos with player 1's right bumper to grab and the left bumper to release
+        if (gamepad1.right_bumper){
+            robot.foundation1.setPosition(robot.FOUNDATION1_GRAB);
+            robot.foundation2.setPosition(robot.FOUNDATION2_GRAB);
+        }
+        if (gamepad1.left_bumper){
+            robot.foundation1.setPosition(robot.FOUNDATION1_START);
+            robot.foundation2.setPosition(robot.FOUNDATION2_START);
+        }
+
+
+
+    }
+
+}
